ac773d28bb65cb723e071839592bf00cfe07135f
Supratim Haldar <40909962+SupratimH@users.noreply.github.com>
2019-07-04 01:18:17 +0530

    DeepArtist Web Application
    Release 1

---
+++ src/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify, json
+from flask_cors import CORS
+from serve import get_model_api
+
+app = Flask(__name__)
+CORS(app) # needed for cross-domain requests, allow everything by default
+model_api = get_model_api()
+
+# default route
+@app.route('/')
+def index():
+    return "Index API"
+
+# HTTP Errors handlers
+@app.errorhandler(404)
+def url_error(e):
+    return """
+    Wrong URL!
+    <pre>{}</pre>""".format(e), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+# API route
+@app.route('/api', methods=['POST'])
+def api():
+	input_url = request.json
+	#input_url = request.args['url']
+	output_data = model_api(input_url)
+	response = jsonify(output_data)
+	return response
+
+if __name__ == '__main__':
+    #app.run(host='0.0.0.0', debug=True)
+	app.run(debug=True)
---
+++ src/serve.py
@@ -0,0 +1,81 @@
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import *
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.initializers import *
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import imageio
+import cv2
+import os
+
+
+def make_model():
+	train_input_shape = (224, 224, 3)
+	n_classes = 11
+	
+	# Base model
+	base_model = ResNet50(weights=None, include_top=False, input_shape=train_input_shape)
+
+	# Add layers at the end
+	X = base_model.output
+	X = Flatten()(X)
+
+	X = Dense(512, kernel_initializer='he_uniform')(X)
+	#X = Dropout(0.5)(X)
+	X = BatchNormalization()(X)
+	X = Activation('relu')(X)
+
+	X = Dense(16, kernel_initializer='he_uniform')(X)
+	#X = Dropout(0.5)(X)
+	X = BatchNormalization()(X)
+	X = Activation('relu')(X)
+
+	output = Dense(n_classes, activation='softmax')(X)
+
+	model = Model(inputs=base_model.input, outputs=output)
+
+	return model
+
+
+def get_model_api():
+	# Create model architecture
+	model = make_model()
+
+	# Load pre-trained weights
+	model_weight_path = os.path.join('model', 'model_deepartist_rn50.h5')
+	model.load_weights(model_weight_path)
+	model._make_predict_function()
+
+	artists = {
+				0: 'Vincent van Gogh',
+				1: 'Edgar Degas',
+				2: 'Pablo Picasso',
+				3: 'Pierre-Auguste Renoir',
+				4: 'Albrecht Durer',
+				5: 'Paul Gauguin',
+				6: 'Francisco Goya',
+				7: 'Rembrandt',
+				8: 'Alfred Sisley',
+				9: 'Titian',
+				10: 'Marc Chagall'
+			  }
+
+	def model_api(input_url):
+		train_input_shape = (224, 224)
+		web_image = imageio.imread(input_url)
+		web_image = cv2.resize(web_image, dsize=train_input_shape, )
+		web_image = image.img_to_array(web_image)
+		web_image /= 255.
+		web_image = np.expand_dims(web_image, axis=0)
+
+		prediction = model.predict(web_image)
+		prediction_probability = np.amax(prediction) * 100
+		prediction_probability = "{0:.2f}%".format(prediction_probability)
+		prediction_idx = np.argmax(prediction)
+
+		output_data = {'artist': artists[prediction_idx], 'probability': prediction_probability}
+
+		return output_data
+
+	return model_api
